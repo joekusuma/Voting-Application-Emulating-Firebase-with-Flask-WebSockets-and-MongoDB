0bd8db827fc64a62239d0617ce90a369318c7a73
joe kusuma <farrelljonathan@rocketmail.com>
2023-03-25 15:00:03 -0700

    updated frontend

--- Flask_Mongodb/dsci551pymongo/main.py
+++ Flask_Mongodb/dsci551pymongo/main.py
@@ -1,46 +1,67 @@
+from flask import Flask, render_template, request, redirect, url_for
+# from pymongo import MongoClient
 from flask import Blueprint, render_template
 
-# from .extensions import mongo
-from pymongo import MongoClient
+# dsci551db = MongoClient(
+#     "mongodb+srv://dsci551:w8QBPtSfunya7Gy0@clusterdsci551.s85edni.mongodb.net/?retryWrites=true&w=majority"
+# )
 
 main = Blueprint("main", __name__)
 
-dsci551db = MongoClient(
-    "mongodb+srv://dsci551:w8QBPtSfunya7Gy0@clusterdsci551.s85edni.mongodb.net/?retryWrites=true&w=majority"
-)
+polls = [    
+    {"id": 1, "title": "What is your favorite color?", "description": "Choose one of the following colors.", "options": ["Red", "Green", "Blue"], "votes": [3, 2, 1]},
+    {"id": 2, "title": "What is your favorite food?", "description": "Choose one of the following foods.", "options": ["Pizza", "Burgers", "Tacos"], "votes": [5, 3, 2]},
+    {"id": 3, "title": "What is your favorite movie?", "description": "Choose one of the following movies.", "options": ["Star Wars", "The Godfather", "The Shawshank Redemption"], "votes": [1, 4, 5]}
+]
 
 
 @main.route("/")
 def index():
-    user_collection = dsci551db.dsci551.users
-    user_collection.insert_one({"name": "Tiger", "age": 24})
-    user_collection.insert_one({"name": "Joe", "age": 22})
-    return "<h1>Added a User!</h1>"
-    # return render_template("index.html")
-
+    # user_collection = dsci551db.dsci551.users
+    # user_collection.insert_one({"name": "Tiger", "age": 24})
+    # user_collection.insert_one({"name": "Joe", "age": 22})
+    return render_template("index.html")
 
 @main.route("/find")
 def find():
-    user_collection = dsci551db.dsci551.users
-    user = user_collection.find_one({"name": "Tiger"})
-    return f"<h1>User: { user['name']} age: {user['age']}</h1>"
-    # return render_template("index.html")
-
+    # user_collection = dsci551db.dsci551.users
+    # user = user_collection.find_one({"name": "Tiger"})
+    return render_template("index.html", user=user)
 
 @main.route("/update")
 def update():
-    user_collection = dsci551db.dsci551.users
-    filter = {"name": "Tiger"}
-    newvalues = {"$set": {"age": 80}}
-    user_collection.update_one(filter, newvalues)
-    return "<h1>Updated User!</h1>"
-    # return render_template("index.html")
-
+    # user_collection = dsci551db.dsci551.users
+    # filter = {"name": "Tiger"}
+    # newvalues = {"$set": {"age": 80}}
+    # user_collection.update_one(filter, newvalues)
+    return render_template("index.html")
 
 @main.route("/delete")
 def delete():
-    user_collection = dsci551db.dsci551.users
-    filter = {"name": "Tiger"}
-    user_collection.delete_one(filter)
-    return "<h1>Deleted User!</h1>"
-    # return render_template("index.html")
+    # user_collection = dsci551db.dsci551.users
+    # filter = {"name": "Tiger"}
+    # user_collection.delete_one(filter)
+    return render_template("index.html")
+
+@main.route("/create-poll", methods=["GET", "POST"])
+def create_poll():
+    if request.method == "POST":
+        title = request.form["title"]
+        description = request.form["description"]
+        polls.append({"title": title, "description": description})
+        return redirect(url_for("main.view_polls"))
+    return render_template("create_poll.html")
+
+@main.route("/view-polls")
+def view_polls():
+    return render_template("view_polls.html", polls=polls)
+
+@main.route("/login")
+def login():
+    return render_template("login.html")
+
+app = Flask(__name__)
+app.register_blueprint(main)
+
+if __name__ == '__main__':
+    app.run(debug=True)
